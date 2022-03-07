2f68ffce1f7c70b2d35c1032cf382cdb645d6ae1
mutkajoakim <mutkajoa@arcada.fi>
2022-03-07 13:52:48 +0200

    Added precentage calc

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,8 @@
 public class Main {
 
 	public static void main(String[] args) {
-			
+		String test = FileHandler.readTextFile("assets/lang-samples/SV.txt");
+		System.out.println(Utils.getPrecentage(test));
 	}
 }
 
--- src/main/java/Utils.java
+++ src/main/java/Utils.java
@@ -1,8 +1,10 @@
+import java.security.Key;
 import java.util.HashMap;
+import java.util.Map;
 
 public class Utils {
     
-    public HashMap<Character, Integer> getFrequency(String input){
+    static HashMap<Character, Integer> getFrequency(String input){
         input = cleanString(input);
         HashMap<Character, Integer> map = new HashMap<Character, Integer>();
         for (Character character : input.toCharArray()) {
@@ -16,13 +18,21 @@ public class Utils {
         return map;
     }
 
-    public String cleanString(String input){
+    static String cleanString(String input){
         input = input.toLowerCase();
         input = input.replaceAll("[^a-zåäöøæéèóòôáàçâêîôûàìíùëïüñß]", "");
         return input;  
     }
 
-    
+    static HashMap<Character, Float> getPrecentage(String input){
+        Integer stringSize = cleanString(input).length();
+        HashMap<Character, Integer> frequencyHashMap = getFrequency(input);
+        HashMap<Character, Float> map = new HashMap<Character, Float>();
+        frequencyHashMap.forEach((Character chara, Integer intg) ->{
+            map.put(chara, ((float)intg/(float)stringSize)*100);
+        });
+        return map;
+    }
 
 
 }
