f43822cd82f5b5d6395fc1652e65ce9eabb431f7
knappeni <knappeni@arcada.fi>
2022-03-15 12:08:19 +0000

    UI class

---
+++ src/main/java/UI.java
@@ -0,0 +1,10 @@
+public class UI {
+
+    public void combineAndRank(String inputString) {
+        var del1 = Utils.nogo(inputString);
+        var del2 = Utils.nogo(inputString);
+        var del3 = Utils.nogo(inputString);
+
+    }
+
+}
